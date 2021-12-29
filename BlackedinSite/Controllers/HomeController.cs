77eeb5525d5e01711552cae43dd21e9a0adedf07
Tassio <tassio.dev@gmail.com>
2021-12-29 12:38:16 -0300

    Primeiro commit do projeto Blackedin

---
+++ BlackedinSite/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using BlackedinSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlackedinSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CadastroProfissional()
+        {
+            return View();
+        }
+
+        public IActionResult CadastroEmpresa()
+        {
+            return View();
+        }
+
+        public IActionResult MatchProfissional()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
